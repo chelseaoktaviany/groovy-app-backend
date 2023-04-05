1c3348e261e3f98fa07e816b47e5420bf6833dc2
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-04-05 09:26:22 +0700

    Translated into indonesian for role permission error message

--- controllers/authController.js
+++ controllers/authController.js
@@ -443,7 +443,7 @@ exports.restrictTo = (...roles) => {
     // roles['admin', 'super-admin'], role='user'
     if (!roles.includes(req.user.role)) {
       return next(
-        new AppError('You do not have permission to perform this action', 403)
+        new AppError('Anda tidak punya izin untuk melakukan tindakan ini', 403)
       );
     }
 
