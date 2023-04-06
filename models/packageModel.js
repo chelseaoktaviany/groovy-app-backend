95e846693ca0765c034912f5a256c68dd17688fc
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-04-06 14:10:20 +0700

    Added package model

---
+++ models/packageModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const packageSchema = new mongoose.Schema({
+  packageName: {
+    type: String,
+    required: [true, 'Mohon isi nama paket internet'],
+  },
+  packageDescription: {
+    type: String,
+    required: [true, 'Mohon isi deskripsi paket internet'],
+  },
+  packagePrice: {
+    type: Number,
+    required: [true, 'Mohon isi harga paket internet'],
+  },
+  packageImage: {
+    type: String,
+  },
+  packageType: {
+    type: String,
+    enum: ['Yearly', 'Monthly'],
+    required: [true, 'Mohon isi tipe paket internet'],
+  },
+});
+
+const Package = mongoose.model('Package', packageSchema);
+
+module.exports = Package;
