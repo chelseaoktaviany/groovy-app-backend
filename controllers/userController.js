541484268b0eece02a57c47a1e6403fa49994cce
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-04-06 12:32:35 +0700

    Added getMe and getUser function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,21 @@
 const User = require('../models/userModel');
 
+// util
+const catchAsync = require('../utils/catchAsync');
+
+// controllers
 const factory = require('./handleFactory');
 
-// memperolehkan semua user (NANTI)
+// memperolehkan semua user
 exports.getAllUsers = factory.getAll(User, 'Berhasil mengakses data pengguna');
 
-// memperolehkan sebuah user (NANTI)
+// memperolehkan sebuah user
+exports.getMe = catchAsync(async (req, res, next) => {
+  req.params.id = req.user.id;
+
+  next();
+});
+
+exports.getUser = factory.getOne(User, 'Success');
 
 // mengubah user (JANGAN mengubah password dengan ini) NANTI
-
-// menghapus user (NANTI)
