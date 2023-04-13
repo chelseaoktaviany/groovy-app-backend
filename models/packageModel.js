6561deeef39ea73433551171b7e33b711d155eec
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-04-13 10:58:16 +0700

    added version key

--- models/packageModel.js
+++ models/packageModel.js
@@ -1,27 +1,28 @@
 const mongoose = require('mongoose');
 
-const packageSchema = new mongoose.Schema({
-  packageName: {
-    type: String,
-    required: [true, 'Mohon isi nama paket internet'],
+const packageSchema = new mongoose.Schema(
+  {
+    packageName: {
+      type: String,
+      required: [true, 'Mohon isi nama paket internet'],
+    },
+    packageDescription: {
+      type: String,
+      required: [true, 'Mohon isi deskripsi paket internet'],
+    },
+    packagePrice: {
+      type: Number,
+      required: [true, 'Mohon isi harga paket internet'],
+    },
+    packageImage: String,
+    packageType: {
+      type: String,
+      enum: ['Yearly', 'Monthly'],
+      required: [true, 'Mohon isi tipe paket internet'],
+    },
   },
-  packageDescription: {
-    type: String,
-    required: [true, 'Mohon isi deskripsi paket internet'],
-  },
-  packagePrice: {
-    type: Number,
-    required: [true, 'Mohon isi harga paket internet'],
-  },
-  packageImage: {
-    type: String,
-  },
-  packageType: {
-    type: String,
-    enum: ['Yearly', 'Monthly'],
-    required: [true, 'Mohon isi tipe paket internet'],
-  },
-});
+  { versionKey: false }
+);
 
 const Package = mongoose.model('Package', packageSchema);
 
