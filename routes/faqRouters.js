a7a5fc7da7db01ed64ca8a048eecda13c98967f5
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-05-19 16:26:30 +0700

    Updated faq routers

--- routes/faqRouters.js
+++ routes/faqRouters.js
@@ -1,33 +1,25 @@
 const express = require('express');
 
 // controllers
-const adminAuthController = require('../controllers/adminAuthController');
+const authController = require('../controllers/authController');
 const faqController = require('../controllers/faqController');
 
 const router = express.Router();
 
 // package route
-router
-  .route('/')
-  .get(faqController.getAllFaqs)
-  .post(
-    adminAuthController.protect,
-    adminAuthController.restrictTo('admin'),
-    faqController.createFaq
-  );
+router.use(authController.protect);
+
+router.get('/', faqController.getAllFaqs);
+
+router.get('/:id', faqController.getFaq);
+
+router.use(authController.restrictTo('admin'));
+
+router.route('/').post(faqController.createFaq);
 
 router
   .route('/:id')
-  .get(faqController.getFaq)
-  .patch(
-    adminAuthController.protect,
-    adminAuthController.restrictTo('admin'),
-    faqController.updateFaq
-  )
-  .delete(
-    adminAuthController.protect,
-    adminAuthController.restrictTo('admin'),
-    faqController.deleteFaq
-  );
+  .patch(faqController.updateFaq)
+  .delete(faqController.deleteFaq);
 
 module.exports = router;
