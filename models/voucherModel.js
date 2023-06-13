e90e256b905b1a085105af32d3a62e71ac8b144d
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-06-13 13:08:37 +0700

    Updated voucher model

--- models/voucherModel.js
+++ models/voucherModel.js
@@ -1,31 +1,44 @@
 const mongoose = require('mongoose');
 
-const voucherSchema = mongoose.Schema({
-  voucherTitle: {
-    type: String,
-    require: [true, 'Mohon isi nama voucher'],
+const voucherSchema = mongoose.Schema(
+  {
+    voucherTitle: {
+      type: String,
+      require: [true, 'Mohon isi nama voucher'],
+    },
+    voucherType: {
+      type: String,
+      require: [true, 'Mohon isi tipe voucher'],
+    },
+    voucherDescription: {
+      type: String,
+      require: [true, 'Mohon isi deskripsi voucher'],
+    },
+    voucherPrice: {
+      type: String,
+      require: [true, 'Mohon isi harga voucher'],
+    },
+    voucherImage: {
+      type: String,
+      require: [true, 'Mohon isi gambar voucher'],
+    },
+    validUntilDate: Date,
+    redeemedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      default: null,
+    },
   },
-  voucherType: {
-    type: String,
-    require: [true, 'Mohon isi tipe voucher'],
-  },
-  voucherDescription: {
-    type: String,
-    require: [true, 'Mohon isi deskripsi voucher'],
-  },
-  voucherPrice: {
-    type: String,
-    require: [true, 'Mohon isi harga voucher'],
-  },
-  voucherImage: {
-    type: String,
-    require: [true, 'Mohon isi gambar voucher'],
-  },
-  redeemDate: {
-    type: Date,
-    default: Date.now(),
-    require: [true, 'Mohon isi tanggal redeem voucher'],
-  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
+
+voucherSchema.pre(/^find/, function (next) {
+  this.populate('redeemedBy').populate({
+    path: 'redeemedBy',
+    select: 'firstName lastName emailAddress nomorHP profileImage',
+  });
+
+  next();
 });
 
 const Voucher = mongoose.model('Voucher', voucherSchema);
