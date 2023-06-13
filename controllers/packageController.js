adcc0ecce1aebc4d9312608549ea0f7c85c4cfe0
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-06-13 13:07:01 +0700

    Updated package controller

--- controllers/packageController.js
+++ controllers/packageController.js
@@ -82,19 +82,45 @@ exports.createPackage = catchAsync(async (req, res, next) => {
 
   sharp(packageImage).resize({ width: 500, height: 500 }).toFile(outputPath);
 
-  const newPackage = await Package.create({
-    packageName: filteredBody.packageName,
-    packageDescription: filteredBody.packageDescription,
-    packagePrice: filteredBody.packagePrice,
-    packageImage: outputPath,
-    packageType: filteredBody.packageType,
-  });
+  if (filteredBody.packageType === 'Monthly') {
+    const packageDateExpiration = new Date(
+      Date.now() + 30 * 24 * 60 * 60 * 1000
+    );
 
-  res.status(201).json({
-    status: 0,
-    msg: 'Berhasil menambahkan data paket internet',
-    data: newPackage,
-  });
+    const newPackage = await Package.create({
+      packageName: filteredBody.packageName,
+      packageDescription: filteredBody.packageDescription,
+      packagePrice: filteredBody.packagePrice,
+      packageImage: outputPath,
+      packageType: filteredBody.packageType,
+      packageDateExpiration,
+    });
+
+    res.status(201).json({
+      status: 0,
+      msg: 'Berhasil menambahkan data paket internet',
+      data: newPackage,
+    });
+  } else if (filteredBody.packageType === 'Yearly') {
+    const packageDateExpiration = new Date(
+      Date.now() + 365 * 24 * 60 * 60 * 1000
+    );
+
+    const newPackage = await Package.create({
+      packageName: filteredBody.packageName,
+      packageDescription: filteredBody.packageDescription,
+      packagePrice: filteredBody.packagePrice,
+      packageImage: outputPath,
+      packageType: filteredBody.packageType,
+      packageDateExpiration,
+    });
+
+    res.status(201).json({
+      status: 0,
+      msg: 'Berhasil menambahkan data paket internet',
+      data: newPackage,
+    });
+  }
 });
 
 // edit package
