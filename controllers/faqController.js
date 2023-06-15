db6ec6ead3d5aba56efd6b0d6638ec35c6213dad
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-06-15 14:06:33 +0700

    Updated faq controller

--- controllers/faqController.js
+++ controllers/faqController.js
@@ -2,6 +2,9 @@ const factory = require('./handleFactory');
 
 const Faq = require('../models/faqModel');
 
+const AppError = require('../utils/appError');
+const catchAsync = require('../utils/catchAsync');
+
 exports.getAllFaqs = factory.getAll(Faq, 'Berhasil mengakses semua data FAQ');
 
 exports.getFaq = factory.getOne(
@@ -18,3 +21,25 @@ exports.deleteFaq = factory.deleteOne(
   Faq,
   'Berhasil menghapus sebuah data FAQ'
 );
+
+exports.disableFaq = catchAsync(async (req, res, next) => {
+  const id = req.params.id;
+
+  const updatedFaq = await Faq.findByIdAndUpdate(
+    id,
+    {
+      isDisabled: true,
+    },
+    { new: true, runValidators: true }
+  );
+
+  // if (updatedFaq) {
+  //   return next(new AppError('No faq found', 404));
+  // }
+
+  res.status(200).json({
+    status: 0,
+    msg: 'Disabled faq successfully',
+    data: updatedFaq,
+  });
+});
