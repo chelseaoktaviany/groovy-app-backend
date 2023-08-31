675a223620a788ebb803946424012935d712b558
chelseaoktaviany <chelseaoktaviany97@gmail.com>
2023-08-31 16:15:33 +0700

    Updated transaction controller

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,4 +1,10 @@
 const { v4: uuidv4 } = require('uuid');
+const midtransClient = require('midtrans-client');
+
+const dotenv = require('dotenv');
+
+// menggunakan dotenv config
+dotenv.config({ path: './config.env' });
 
 const Package = require('../models/packageModel');
 const User = require('../models/userModel');
@@ -8,130 +14,158 @@ const catchAsync = require('../utils/catchAsync');
 const factory = require('./handleFactory');
 const AppError = require('../utils/appError');
 
+const core = new midtransClient.CoreApi({
+  isProduction: false,
+  serverKey: process.env.SB_MIDTRANS_SERVER_KEY,
+  clientKey: process.env.SB_MIDTRANS_CLIENT_KEY,
+});
+
+exports.getCreditCard = catchAsync(async (req, res, next) => {
+  try {
+    const parameter = {
+      card_number: req.body.card_number,
+      card_exp_month: req.body.card_exp_month,
+      card_exp_year: req.body.card_exp_year,
+      card_ccv: req.body.card_ccv,
+      client_key: core.apiConfig.clientKey,
+    };
+
+    //code here
+    core
+      .cardToken(parameter)
+      .then((cardResponse) => {
+        console.log('Card response:', JSON.stringify(cardResponse));
+
+        const dataCard = {
+          response: JSON.stringify(cardResponse),
+        };
+
+        res.status(200).json({
+          status: 0,
+          msg: 'Retrieved card response successfully',
+          dataCard,
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
 exports.checkoutProduct = catchAsync(async (req, res, next) => {
-  const { packageId } = req.params;
+  try {
+    const { packageId } = req.params;
 
-  // get user
-  const user = await User.findById(req.user.id);
+    // get user
+    const user = await User.findById(req.user.id);
 
-  if (!user) {
-    return next(new AppError('User not found', 404));
+    if (!user) {
+      return next(new AppError('User not found', 404));
+    }
+
+    // get package
+    const existedPackage = await Package.findById(packageId);
+
+    if (!existedPackage) {
+      return next(new AppError('Package not found', 404));
+    }
+
+    const transactionId = uuidv4();
+
+    const parameter = {
+      transaction_detail: {
+        order_id: transactionId,
+        gross_amount: existedPackage.packagePrice,
+      },
+      customer_details: {
+        first_name: user.firstName,
+        last_name: user.lastName,
+        email: user.emailAddress,
+        phone: user.nomorHP,
+      },
+      name: existedPackage.packageName,
+      amount: existedPackage.packagePrice,
+      currency: 'IDR',
+      payment_type: 'credit_card',
+      credit_card: {
+        secure: true,
+        save_card: true,
+      },
+      token: '521111gmWqMegyejqCQmmopnCFRs1117', // ambil dari get credit card
+      schedule: {
+        interval: 1,
+        interval_unit: 'month',
+        max_interval: 12,
+        // start_time: existedPackage.packageNextPayment, // solution?
+        start_time: '2023-08-31 15:07:00 +0700',
+      },
+    };
+
+    core
+      .createSubscription(parameter)
+      .then((transaction) => {
+        // transaction token
+        const transactionToken = transaction.token;
+
+        console.log('transactionToken:', transactionToken);
+
+        const dataSubscription = JSON.stringify(transaction);
+
+        const newTransaction = new Transaction({
+          transactionId,
+          package: existedPackage._id,
+          user: user._id,
+          payerEmail: user.emailAddress,
+          amount: existedPackage.packagePrice,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          responseMidtrans: dataSubscription,
+        });
+
+        newTransaction.save({ validateBeforeSave: false });
+
+        res.status(200).json({
+          status: 0,
+          msg: `Invoice created with ID: ${transactionId}`,
+          data: newTransaction,
+          transactionToken,
+        });
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  } catch (err) {
+    console.log(err.message);
   }
+});
 
-  // get package
-  const existedPackage = await Package.findById(packageId);
+exports.verifyPaymentTransaction = catchAsync(async (req, res, next) => {
+  // code here
+});
 
-  if (!existedPackage) {
-    return next(new AppError('Package not found', 404));
-  }
+exports.cancelProduct = catchAsync(async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-  const transactionId = uuidv4();
+    core.disableSubscription(id).then((response) => {
+      console.log('Response:', JSON.stringify(response));
 
-  const transaction = new Transaction({
-    transactionId,
-    package: existedPackage._id,
-    user: user._id,
-    payerEmail: user.emailAddress,
-    amount: existedPackage.packagePrice,
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  });
+      const dataSubscription = {
+        response: JSON.stringify(response),
+      };
 
-  await transaction.save({ validateBeforeSave: false });
-
-  // generate payment link with xendit
-  const { Invoice } = xendit;
-  const invoiceOptions = {};
-  const i = new Invoice(invoiceOptions);
-
-  i.createInvoice({
-    externalID: transactionId,
-    payerEmail: user.emailAddress,
-    description: `Purchase for ${existedPackage.packageName}`,
-    currency: transaction.currency,
-    amount: existedPackage.packagePrice,
-    invoiceDuration: 24,
-    forUserID: user._id,
-  })
-    .then((id) => {
       res.status(200).json({
         status: 0,
-        msg: `Invoice created with ID: ${id}`,
+        msg: 'Berhasil membatalkan subscription',
+        dataSubscription,
       });
-    })
-    .catch((err) => {
-      console.log('Failed to create invoice', err);
-      return next(new AppError('Failed to create invoice', 500));
     });
-});
-
-exports.createEWalletCharge = catchAsync(async (req, res, next) => {
-  const { packageId } = req.params;
-
-  // get user
-  const user = await User.findById(req.user.id);
-
-  if (!user) {
-    return next(new AppError('User not found', 404));
+  } catch (err) {
+    console.log(err.message);
   }
-
-  // get package
-  const existedPackage = await Package.findById(packageId);
-
-  if (!existedPackage) {
-    return next(new AppError('Package not found', 404));
-  }
-
-  const x = new require('xendit-node')({
-    secretKey: process.env.XENDIT_API_KEY,
-  });
-
-  const transactionId = uuidv4();
-
-  const { EWallet } = x;
-  const ewalletSpecificOptions = {};
-  const ew = new EWallet(ewalletSpecificOptions);
-
-  const resp = await ew.createEWalletCharge({
-    referenceID: transactionId,
-    amount: existedPackage.packagePrice,
-    currency: 'IDR',
-    checkoutMethod: 'TOKENIZED_PAYMENT',
-    channelCode: ['ID_SHOPEEPAY', 'ID_OVO', 'ID_DANA', 'ID_LINKAJA'],
-    channelProperties: {
-      phone_number: user.nomorHP,
-      successRedirectURL: 'https://dashboard.xendit.co/register/1',
-      failureRedirectURL: '',
-      redeem_points: 'REDEEM_NONE',
-    },
-    metadata: {
-      branch_code: 'tree_branch',
-    },
-  });
-
-  console.log(resp);
-
-  const transaction = new Transaction({
-    transactionId,
-    package: existedPackage._id,
-    user: user._id,
-    payerEmail: user.emailAddress,
-    amount: existedPackage.packagePrice,
-    paymentStatus: 'process',
-    createdAt: new Date(),
-    updatedAt: new Date(),
-  });
-
-  await transaction.save();
-
-  return res.status(201).json({
-    status: 0,
-    msg: 'You have successfully made a transaction',
-    data: transaction,
-  });
 });
-
-exports.verifyPaymentTransaction = catchAsync(async (req, res, next) => {});
 
 exports.getAllTransactions = factory.getAll(
   Transaction,
